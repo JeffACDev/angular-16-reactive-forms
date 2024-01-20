5c4edb922225f5cc3369764f598571268c0eb164
Jeff Anderson <jeffacdev@gmail.com>
2024-01-20 15:42:57 -0600

    form groups

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,35 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
   templateUrl: './edit-contact.component.html',
-  styleUrls: ['./edit-contact.component.scss']
+  styleUrls: ['./edit-contact.component.scss'],
 })
 export class EditContactComponent implements OnInit {
-firstName= new FormControl();
-lastName= new FormControl();
-dateOfBirth= new FormControl();
-favoritesRanking= new FormControl();
+  contactForm = new FormGroup({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    dateOfBirth: new FormControl(),
+    favoritesRanking: new FormControl(),
+  });
 
-  constructor(private route: ActivatedRoute, private contactsService:ContactsService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private contactsService: ContactsService
+  ) {}
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
-    if (!contactId) return
+    if (!contactId) return;
 
-    this.contactsService.getContact(contactId).subscribe((contact)=> {
-      if(!contact) return;
+    this.contactsService.getContact(contactId).subscribe((contact) => {
+      if (!contact) return;
 
-      this.firstName.setValue(contact.firstName);
-      this.lastName.setValue(contact.lastName);
-      this.dateOfBirth.setValue(contact.dateOfBirth);
-      this.favoritesRanking.setValue(contact.favoritesRanking);
+      this.contactForm.controls.firstName.setValue(contact.firstName);
+      this.contactForm.controls.lastName.setValue(contact.lastName);
+      this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
+      this.contactForm.controls.favoritesRanking.setValue(
+        contact.favoritesRanking
+      );
     });
   }
 
-  saveContact() {
-
-  }
+  saveContact() {}
 }
